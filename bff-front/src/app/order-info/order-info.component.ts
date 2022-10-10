7f28b8ff070e86bb7204ee712a99ab05315d2ee6
YannisFalco <yannis.falco@etu.univ-cotedazur.fr>
2022-10-10 11:52:33 +0200

    Add payment in front #5

--- bff-front/src/app/order-info/order-info.component.ts
+++ bff-front/src/app/order-info/order-info.component.ts
@@ -4,6 +4,7 @@ import {OrderInfoService} from "../../services/order-info.service";
 import {orderInfoList} from "../../mocks/orderinfo.mock";
 import {ActivatedRoute, Router} from "@angular/router";
 import {OrderService} from "../../services/order.service";
+import {PaymentService} from "../../services/payment.service";
 
 @Component({
   selector: 'app-order-info',
@@ -15,7 +16,8 @@ export class OrderInfoComponent implements OnInit {
   orderInfo: OrderInfo = orderInfoList;
   tableId: number = 0;
 
-  constructor(private orderService: OrderService, private orderInfoService: OrderInfoService, private route: ActivatedRoute, private router: Router) {
+
+  constructor(private orderService: OrderService ,private paymentServer:PaymentService, private orderInfoService: OrderInfoService, private route: ActivatedRoute, private router: Router) {
     this.orderInfoService.orderInfo$.subscribe((orderInfo: OrderInfo) => {
       this.orderInfo = orderInfo;
     });
@@ -32,6 +34,15 @@ export class OrderInfoComponent implements OnInit {
 
   onAddClick() {
     this.router.navigate(["/menu-list/" + this.tableId]);
+  }
+
+  onPaymentClick() {
+    try {
+      this.paymentServer.pay(this.tableId);
+    }
+    catch (e){
+      this.router.navigate(["/table-list"]);
+    }
   }
 
   onServeClick() {
